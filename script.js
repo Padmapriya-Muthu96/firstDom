e631de4862bb9cf8ce2cef000759645e3c749772
Padmapriya-Muthu96 <121947075+Padmapriya-Muthu96@users.noreply.github.com>
2023-04-12 10:55:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+// var element=document.createElement("div");
+// // console.log(elememnt);
+// element.innerHTML="this is div tag";
+// document.body.append(element);
+
+
+// var container=document.createElement("div");
+// container.className="container";
+
+// var row=document.createElement("div");
+// row.className="row";
+
+
+// var col=document.createElement("div");
+// col.className="col";
+
+// row.append(col);
+// container.append(row);
+// document.body.append(container);
+
+function check(){
+    let acc=document.getElementById("firstname").value;
+    console.log(acc);
+    let acc1=document.getElementById("Lastname").value;
+    console.log(acc1);
+    let acc2=document.getElementById("Emailaddress").value;
+    console.log(acc2);
+    let acc3=document.getElementById("Password").value;
+    console.log(acc3);
+    let acc4=document.getElementById("Mobilenumber").value;
+    console.log(acc4);
+    
+}
